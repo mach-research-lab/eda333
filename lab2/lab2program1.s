177c25dc7e6bf1bbd40139c25a00e9f792d59758
cicero-a <136462951+cicero-a@users.noreply.github.com>
2026-03-31 22:47:39 +0200

    Update lab2program1.s

--- lab2/lab2program1.s
+++ lab2/lab2program1.s
@@ -1,4 +1,4 @@
-addi t0,  zero, 1    # t0 ← 1
-add  t1,  t0, t0   # t1 ← t0 + t0
-add  t2, t0, t0   # t2 ← t0 + t0
-add  t3, t0, t0   # t3 ← t0 + t0
+addi t3,  zero, 1    # t3 ← 1
+add  t4,  t3, t3   # t4 ← t3 + t3
+add  t5, t3, t3   # t5 ← t3 + t3
+add  t6, t3, t3   # t6 ← t3 + t3
